0be504feef51064f471783c138e584f09076b78b
zxl3651 <zxl3651@naver.com>
2023-10-27 05:15:58 +0900

    Firebase 토이프로젝트 생성

---
+++ FirebaseAPI_SwiftUI/FirebaseAPI_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseAPI_SwiftUI
+//
+//  Created by 이성현 on 2023/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseAPI_SwiftUI/FirebaseAPI_SwiftUI/FirebaseAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAPI_SwiftUIApp.swift
+//  FirebaseAPI_SwiftUI
+//
+//  Created by 이성현 on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
